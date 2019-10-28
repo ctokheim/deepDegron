890436a8323daec54a196e1cbb77c3cc83ffee02
ctokheim <ctokheim@mail.dfci.harvard.edu>
2019-10-28 15:52:20 -0400

    Updated unit test script

--- tests/test_ctnnb1_mutation.py
+++ tests/test_ctnnb1_mutation.py
@@ -1,9 +1,21 @@
 import deepDegron.statistical_test as test
+import os
+import sys
+file_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(file_dir, '../'))
 
 
 def test_ctnnb1_main():
-    opts ={
-
+    opts = {
+        'input': os.path.join(file_dir, 'data/CTNNB1_mutations_ucec.txt'),
+        'degrons': None,
+        'sites': os.path.join(file_dir, 'data/ctnnb1_phosphodegron_sites.txt'),
+        'flank': 3,
+        'processes': 0,
+        'cterm_models': None,
+        'nterm_models': None,
+        'output': os.path.join(file_dir, 'output/ctnnb1_phospho_results.txt')
     }
+    test.main(opts)
 
     assert 1==1
