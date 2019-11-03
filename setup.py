fbbf845b2ca39e33fe23b4b417c3962e13ececc4
Collin Tokheim <collintokheim@gmail.com>
2019-11-02 22:27:34 -0400

    Update package deps

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='deepDegron',
         url=URL,
         packages=PACKAGES,
         license='Apache License, Version 2.0',
-        install_requires=['numpy', 'scipy', 'pandas', 'pysam'],
+        install_requires=['numpy', 'scipy', 'pandas', 'sklearn', 'pyensembl', 'varcode', 'tensorflow', 'keras'],
         package_data={
         },
         entry_points={
